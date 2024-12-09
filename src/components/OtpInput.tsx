5ee0a3ca7255ee0d4279635dc9b7fca04bdb4b5f
souraj99 <sourajghosh9@gmail.com>
2024-12-09 20:42:15 +0530

    handelClear fn added

--- src/components/OtpInput.tsx
+++ src/components/OtpInput.tsx
@@ -1,93 +1,116 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
 import { OtpInputProps } from "./types";
 import "./OtpInput.css";
 
-const OtpInput: React.FC<OtpInputProps> = ({
-  length = 4,
-  onOtpSubmit = () => {},
-  inputClassName = "otpInput",
-  containerClassName = "otpContainer",
-  disabled = false,
-  ...inputProps
-}) => {
-  const [otp, setOtp] = useState<string[]>(Array(length).fill(""));
-  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
-
-  useEffect(() => {
-    if (inputRefs.current[0] && !disabled) {
-      inputRefs.current[0].focus();
-    }
-  }, [disabled]);
-
-  const handleChange = (
-    index: number,
-    event: React.ChangeEvent<HTMLInputElement>
+const OtpInput = forwardRef<
+  { reset: () => void }, // Ref type: the object with the reset method
+  OtpInputProps
+>(
+  (
+    {
+      length = 4,
+      onOtpSubmit = () => {},
+      inputClassName = "otpInput",
+      containerClassName = "otpContainer",
+      disabled = false,
+      ...inputProps
+    },
+    ref
   ) => {
-    const value = event.target.value;
+    const [otp, setOtp] = useState<string[]>(Array(length).fill(""));
+    const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
 
-    if (isNaN(Number(value))) return;
-
-    const newOtp = [...otp];
-    newOtp[index] = value.substring(value.length - 1);
-    setOtp(newOtp);
-
-    const combinedOtp = newOtp.join("");
-    if (combinedOtp.length === length) {
-      onOtpSubmit(combinedOtp);
-    }
-
-    if (value && index < length - 1) {
-      inputRefs.current[index + 1]?.focus();
-    }
-
-    if (!value && index > 0) {
-      inputRefs.current[index - 1]?.focus();
-    }
-  };
-
-  const handleClick = (index: number) => {
-    inputRefs.current[index]?.setSelectionRange(1, 1);
-    if (index > 0 && !otp[index - 1]) {
-      const firstEmptyIndex = otp.findIndex((val) => !val);
-      if (firstEmptyIndex !== -1) {
-        inputRefs.current[firstEmptyIndex]?.focus();
+    useEffect(() => {
+      if (inputRefs.current[0] && !disabled) {
+        inputRefs.current[0].focus();
       }
-    }
-  };
+    }, [disabled]);
 
-  const handleKeyDown = (
-    index: number,
-    event: React.KeyboardEvent<HTMLInputElement>
-  ) => {
-    if (disabled) return;
-    if (event.key === "Backspace" && !otp[index] && index > 0) {
-      inputRefs.current[index - 1]?.focus();
-    }
-  };
+    const handleChange = (
+      index: number,
+      event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      const value = event.target.value;
 
-  useEffect(() => {
-    onOtpSubmit(otp.join(""));
-  }, [otp, onOtpSubmit]);
+      if (isNaN(Number(value))) return;
 
-  return (
-    <div className={containerClassName}>
-      {otp.map((value, index) => (
-        <input
-          key={index}
-          type="text"
-          ref={(input) => (inputRefs.current[index] = input)}
-          value={value}
-          onChange={(e) => handleChange(index, e)}
-          onClick={() => handleClick(index)}
-          onKeyDown={(e) => handleKeyDown(index, e)}
-          className={inputClassName}
-          disabled={disabled}
-          {...inputProps}
-          inputMode="numeric"
-        />
-      ))}
-    </div>
-  );
-};
+      const newOtp = [...otp];
+      newOtp[index] = value.substring(value.length - 1);
+      setOtp(newOtp);
+
+      const combinedOtp = newOtp.join("");
+      if (combinedOtp.length === length) {
+        onOtpSubmit(combinedOtp);
+      }
+
+      if (value && index < length - 1) {
+        inputRefs.current[index + 1]?.focus();
+      }
+
+      if (!value && index > 0) {
+        inputRefs.current[index - 1]?.focus();
+      }
+    };
+
+    const handleClick = (index: number) => {
+      inputRefs.current[index]?.setSelectionRange(1, 1);
+      if (index > 0 && !otp[index - 1]) {
+        const firstEmptyIndex = otp.findIndex((val) => !val);
+        if (firstEmptyIndex !== -1) {
+          inputRefs.current[firstEmptyIndex]?.focus();
+        }
+      }
+    };
+
+    const handleKeyDown = (
+      index: number,
+      event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+      if (disabled) return;
+      if (event.key === "Backspace" && !otp[index] && index > 0) {
+        inputRefs.current[index - 1]?.focus();
+      }
+    };
+
+    const handleReset = () => {
+      setOtp(Array(length).fill(""));
+      inputRefs.current[0]?.focus(); 
+    };
+
+    useImperativeHandle(ref, () => ({
+      reset: handleReset,
+    }));
+
+    useEffect(() => {
+      onOtpSubmit(otp.join(""));
+    }, [otp, onOtpSubmit]);
+
+    return (
+      <div className={containerClassName}>
+        {otp.map((value, index) => (
+          <input
+            key={index}
+            type="text"
+            ref={(input) => (inputRefs.current[index] = input)}
+            value={value}
+            onChange={(e) => handleChange(index, e)}
+            onClick={() => handleClick(index)}
+            onKeyDown={(e) => handleKeyDown(index, e)}
+            className={inputClassName}
+            disabled={disabled}
+            {...inputProps}
+            inputMode="numeric"
+          />
+        ))}
+      </div>
+    );
+  }
+);
 
 export default OtpInput;
